49e9dad0234eef543f04877deba0082d087b8661
yagmurbasoglu <108950459+yagmurbasoglu@users.noreply.github.com>
2022-08-15 12:29:22 +0300

    Add files via upload

---
+++ ElmasCizim.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ElmasCizim {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Basamak Sayisini Giriniz : ");
+        int n = input.nextInt();
+
+        for (int i =1;i<=n;i++){
+            for (int k =0; k<=(n-i);k++){
+                System.out.print(" ");
+            }
+        for (int j =1 ; j<= (2*i)-1;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+        }
+        for (int i=n ;i>1;i--){
+            for (int k=n; k>=(i-1);k--){
+                System.out.print(" ");
+            }
+        for (int j = 1; j <= 2*i-3;j++) {
+            System.out.print("*");
+        }
+            System.out.println();
+        }
+        }
+    }
